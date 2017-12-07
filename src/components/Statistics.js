3ee2aada28e39c708e9a61e6da5977704983549b
marshalpaterson <marshal.paterson@gmail.com>
2017-12-08 10:58:52 +1100

    Adding new website

---
+++ src/components/Statistics.js
@@ -0,0 +1,20 @@
+import React, { Component, PropTypes } from 'react';
+
+export default class Statistics extends Component {
+  render() {
+    // Benchmarks are calculated from the time it takes to get from app init (src/index.js)
+    // to render of containers/App.js.
+    const { benchmark } = this.props;
+
+    return (
+      <div className="statistics">
+        {/* React.js rendered this page in {benchmark} milliseconds. Refresh to benchmark again. */}
+        Experience in developing across multiple coding languages with a firm belief in the importance of programming logic and team leadership. Interests are in building high-performance teams with Agile principles. Focused on finding solutions for software development with UML Modelling, Class Diagrams, Sequence Diagrams, Use Case Diagrams, design visualisations, drawing detail blueprints, writing specifications and explaining viewpoints. A Polyglot Engineer with current development stack is Javascript, React, React Native, Redux, Flux, Webpack, Node.js, .NET Core, C#, Java and Objective-C. plus Angular 2 and above. 
+      </div>
+    );
+  }
+}
+
+Statistics.propTypes = {
+  benchmark: PropTypes.number.isRequired
+};
---
+++ src/data/personalInfo.js
@@ -0,0 +1,10 @@
+const personalInfoData = {
+  name: 'Marshal Paterson',
+  linkedIn: 'https://www.linkedin.com/',
+  github: 'https://github.com/',
+  angelList: 'https://angel.co/',
+  email: 'mailto:you@gmail.com',
+  resume: 'https://www.dropbox.com/'
+};
+
+export { personalInfoData };
